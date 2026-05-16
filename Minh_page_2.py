e947cda207cfe9d5618b3a556d1b14fd5112f76b
Conor Le <s4203426@rmit.edu.vn>
2026-05-16 22:13:11 +0700

    set the hover to the filter box and the result

--- Minh_page_2.py
+++ Minh_page_2.py
@@ -172,7 +172,7 @@ def get_page_html(form_data):
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vaccination Data - Vaccination & Infection Tracker</title>
     <link rel="stylesheet" href="navbar.css">
-    <link rel="stylesheet" href="Minh_page_2.css?v=5">
+    <link rel="stylesheet" href="Minh_page_2.css">
     <link rel="stylesheet" href="pagination_bar.css">
     <link rel="stylesheet" href="footer.css">
 </head>
